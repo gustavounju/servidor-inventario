fc03216f6f8f2270a8f1604a4a515dc83a43c9a1
Gustavo Elias Murad <24217436@fi.unju.edu.ar>
2026-03-29 22:55:11 -0300

    Añadido script para generar certificados SSL locales

---
+++ generate_certs.py
@@ -0,0 +1,17 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+import datetime
+import ipaddress
+
+key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+subject = issuer = x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, u"192.168.1.8")])
+cert = x509.CertificateBuilder().subject_name(subject).issuer_name(issuer).public_key(key.public_key()).serial_number(x509.random_serial_number()).not_valid_before(datetime.datetime.utcnow()).not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365)).add_extension(x509.SubjectAlternativeName([x509.IPAddress(ipaddress.IPv4Address("192.168.1.8"))]), critical=False).sign(key, hashes.SHA256())
+
+with open("cert.pem", "wb") as f:
+    f.write(cert.public_bytes(serialization.Encoding.PEM))
+with open("key.pem", "wb") as f:
+    f.write(key.private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm=serialization.NoEncryption()))
+print("SSL Certificates Generated Successfully.")
